37cf3d459de940876b5fd41e7d9a3d83da82c57a
Marius Fagerland <mariusfagerland@yahoo.no>
2019-02-16 17:36:01 +0100

    Implement login/register tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+const AppContainer = styled.div`
+`;
+
+const AppHeader = styled.div`
+  padding: 1rem;
+  height: 3rem;
+  background-color: cornflowerblue;
+  color: white;
+`;
+
+const AppText = styled.div`
+  margin: auto;
+  width: 40%;
+  text-align: center;
+  font-size: 3rem;
+`;
+
+const UserContainer = styled.div`
+  margin: 1rem auto;
+  border: 1px solid cornflowerblue;
+  width: 50%;
+`;
+
+
+const LoginTabButton = styled.button`
+  color: cornflowerblue;
+  background-color: white;
+  border: none;
+  width: 50%;
+`;
+  
+
+const RegisterTabButton = styled.button`
+  color: cornflowerblue;
+  background-color: white;
+  border: 2px solid cornflowerblue;
+  width: 50%;
+`;
+
+const TabContainer = styled.div`
+  width: 100%;
+  padding: 0;
+`;
+
+function App() {
+  return(
+    <div>
+      <AppHeader>
+        <AppText>
+          User service
+        </AppText>
+      </AppHeader>
+      <UserContainer>
+        <TabContainer>
+          <LoginTabButton>Login</LoginTabButton>
+          <RegisterTabButton>Register</RegisterTabButton>
+        </TabContainer>
+        placeholder
+      </UserContainer>
+    </div>
+  );
 }
 
 export default App;
