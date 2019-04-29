949a8b1d72515f040dc6a54b42a216f3433e3c51
Marius Fagerland <mariusfagerland@yahoo.no>
2019-04-29 04:11:21 +0200

    Implement login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
-import { LoginButton } from './Buttons';
-import { Input } from './Inputs';
+import styled from "styled-components";
+import { LoginButton } from "./Buttons";
+import { Input } from "./Inputs";
+import { API_URL } from "../AppConfig";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <div>
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
-            <LoginButton>Login</LoginButton>
-        </div>
-    )
+  function changeUsername(event) {
+    setUsername(event.target.value);
+  }
+
+  function changePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function onLogin() {
+    fetch(API_URL + "/api/auth/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        localStorage.setItem('token', res.token);
+        window.location.replace('/');
+      });
+  }
+
+  return (
+    <div>
+      <Input
+        placeholder="Username"
+        value={username}
+        onChange={changeUsername}
+      />
+      <Input
+        placeholder="Password"
+        value={password}
+        onChange={changePassword}
+      />
+      <LoginButton onClick={onLogin}>Login</LoginButton>
+    </div>
+  );
 }
 
 export default Login;
